fc66c144924f4e685d727fd20b513191d70ff730
Donsin <[donsinalan@hotmail.com>
2020-07-06 11:37:18 +0200

    Configuration NLog OK

--- Initialisation/Initialisation/Program.cs
+++ Initialisation/Initialisation/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NLog;
 namespace Initialisation
 {
     class Program
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            try
+            {
+                Logger.Info("Hello world");
+                System.Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Goodbye cruel world");
+            }
+
         }
     }
 }
