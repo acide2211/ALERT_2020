c1d112c1321a8626de6a904a157f19733c343ff2
Donsin <[donsinalan@hotmail.com>
2020-07-06 16:12:15 +0200

    1_Avant install NLog

---
+++ Initialisation/BusinessToDBAlert/ManagerDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessToDBAlert
+{
+    /***Classe qui permet de faire la gestion de la base de données
+     * 
+     */
+
+    public class ManagerDB
+    {
+        #region Déclaration des variables 
+        //Variable qui permet de faire la connexions a la base de données
+        private DataClassesALERTDataContext _AlertDBContext;
+        //dictionaire qui contient les informations de configuration
+        private Dictionary<String, String> _private;
+        #endregion
+
+        public AlertManager()
+        {
+        }
+
+    }
+}
