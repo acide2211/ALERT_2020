f8f693f3515f91658673f6d4095fd93946b955fb
Donsin <donsinalainhotmail.com>
2020-08-19 15:01:11 +0200

    Creatuib des planning

---
+++ Initialisation/Création_Planning/Program.cs
@@ -0,0 +1,24 @@
+﻿using BusinessToDBAlert;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Création_Planning
+{
+    class Program
+    {
+        //Variable qui permet de crée les logs
+
+
+        static void Main(string[] args)
+        {   
+            NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+            ManagerDB managerDB;
+           
+            managerDB = ManagerDB.getInstance(Logger);
+            Utile utile = new Utile(managerDB, Logger);
+        }
+    }
+}
---
+++ Initialisation/Création_Planning/Utile.cs
@@ -0,0 +1,26 @@
+﻿using BusinessToDBAlert;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Création_Planning
+{
+    public class Utile
+    {
+        /// <summary>
+        /// Attribut qui permet de faire les interaction avec la base de donnée
+        /// </summary>
+        private readonly ManagerDB _managerDB;
+
+        /// <summary>
+        /// Attribut qui permet de faire les log
+        /// </summary>
+        private readonly NLog.Logger _logger;
+        public Utile(ManagerDB managerDB,  NLog.Logger logger)
+        {
+
+        }
+    }
+}
